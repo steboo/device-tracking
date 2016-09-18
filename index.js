dfe98adfd4c3773ae3ac0895b329e5566a7bf479
Stephen Booher <stephen.booher@gmail.com>
2016-09-17 19:15:35 -0600

    add prod index.js with serve-static ref

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var express = require('express');
+var ss = require('serve-static');
 var app = express();
-app.use(express.static('public'));
+var serve = ss('public', { 'index': false });
+app.use(serve);
+
+app.get('/', function (req, res) {
+	res.redirect('/device-tracking/index.html');
+});
+
+app.listen(21215, function () {
+	console.log('Listening');
+});
