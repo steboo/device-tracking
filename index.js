310f68f9e3e70a646b4807fa28afb63cc00c10af
Stephen Booher <stephen.booher@gmail.com>
2016-09-18 22:14:49 -0700

    es6

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-var express = require('express'),
-    cookieParser = require('cookie-parser'),
-    redis = require('redis'),
-    serveStatic = require('serve-static'),
-    uuid = require('node-uuid');
+'use strict';
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const redis = require('redis');
+const serveStatic = require('serve-static');
+const uuid = require('node-uuid');
 
 const WEB_PORT = 21215;
 const REDIS_PORT = 22982;
 
-var app = express(),
-    client = redis.createClient(REDIS_PORT);
+const app = express();
+const client = redis.createClient(REDIS_PORT);
 
 app.use(cookieParser());
 
 // Need /app_path to redirect to /app_path/. It wasn't an option to adjust the
 // web server config on the current production host, so these gymnastics will
 // do.
-app.use(serveStatic('public', { 'index': false }));
+app.use(serveStatic('public', { index: false }));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.redirect('/device-tracking/index.html');
 });
 
 // Could use the traditional tracking pixel here
-app.get('/track.txt', function (req, res) {
-  var trackingID = '';
-  var gen = !!req.query.v;
+app.get('/track.txt', (req, res) => {
+  const gen = !!req.query.v;
+  let trackingID = '';
 
   if (req.cookies.key) {
-    // All requests should have a key if WebRTC is enabled
-    key = req.cookies.key;
-    var keyArr = key.split(',');
+    // All requests should have a device key
+    let deviceKey = req.cookies.key;
+    let keyArr = deviceKey.split(',');
+    let altKey;
 
     // sometimes WebRTC doesn't report the public IP.
     // don't know why yet. here's a workaround i made
-    if (keyArr.length >= 2 && keyArr[1] == '') {
-      var ip = req.headers['x-forwarded-for'] || 
+    if (keyArr.length >= 2 && keyArr[1] === '') {
+      let ip = req.headers['x-forwarded-for'] ||
          req.connection.remoteAddress;
-      
-      ipv = ip.split(':');
+      const ipv = ip.split(':');
+
       if (ip.length > 0) {
         ip = ipv[ipv.length - 1].split(',')[0];
       }
+
       keyArr[1] = ip;
-      key = keyArr.join(',');
+      deviceKey = keyArr.join(',');
     }
 
-    var altKey = key;
-    keyArr = key.split(',');
+    altKey = deviceKey;
+    keyArr = deviceKey.split(',');
 
     // remove private IP (for browsers that don't support WebRTC)
     if (keyArr.length >= 1) {
@@ -57,25 +60,25 @@ app.get('/track.txt', function (req, res) {
     if (req.cookies.trackingID) {
       // Set a new key if necessary
       trackingID = req.cookies.trackingID;
-      console.log('Updating redis...', key, trackingID);
-      client.set(key, req.cookies.trackingID);
-      if (key != altKey) {
+      console.log('Updating redis...', deviceKey, trackingID);
+      client.set(deviceKey, req.cookies.trackingID);
+      if (deviceKey !== altKey) {
         client.set(altKey, req.cookies.trackingID);
       }
     } else {
-      console.log('Querying redis...', key);
-      return client.get(key, function (err, reply) {;
+      console.log('Querying redis...', deviceKey);
+      return client.get(deviceKey, (err, reply) => {
         if (reply) {
-          var trackingID = reply;
+          trackingID = reply;
         }
 
         if (!trackingID && gen) {
           // We have no record of key, so generate new tracking ID.
           // Hopefully this is their first visit.
           trackingID = uuid.v4();
-          console.log('Generating new key (1)', trackingID);
-          client.set(key, trackingID);
-          if (key != altKey) {
+          console.log('Generating new tracking ID (1)', trackingID);
+          client.set(deviceKey, trackingID);
+          if (deviceKey !== altKey) {
             client.set(altKey, trackingID);
           }
         }
@@ -88,7 +91,7 @@ app.get('/track.txt', function (req, res) {
   } else if (gen) {
     // Create a new trackingID if nothing is present
     trackingID = uuid.v4();
-    console.log('Generating new key (2)', trackingID);
+    console.log('Generating new tracking ID (2)', trackingID);
   }
 
   res.header('Content-Type', 'text/plain');
@@ -100,6 +103,6 @@ app.get('/track.txt', function (req, res) {
   res.end(trackingID);
 });
 
-app.listen(WEB_PORT, function () {
-  console.log('Listening on port ' + WEB_PORT);
+app.listen(WEB_PORT, () => {
+  console.log('Listening on port ', WEB_PORT);
 });
